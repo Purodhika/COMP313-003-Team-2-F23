6abfa11ca8b8e18b16137e1f053cad6fb1695240
puneet946 <146029461+puneet946@users.noreply.github.com>
2023-11-18 15:06:30 -0500

    Updating the NavBar

--- bookepedia-frontend-main/src/components/NavBar.js
+++ bookepedia-frontend-main/src/components/NavBar.js
@@ -8,11 +8,9 @@ import { useNavigate } from "react-router-dom";
 
 import React from "react";
 import accountContext from "./userAccounts/accountContext";
-//import NavDropdown from "react-bootstrap/NavDropdown";
-
 import logo from "./media/bookepedia.gif";
 
-function NavBar(props) {
+function NavBar() {
   let navigate = useNavigate();
   const { loggedIn, setLoggedIn, userType, setUserType, setUserEmail } =
     React.useContext(accountContext);
@@ -29,7 +27,10 @@ function NavBar(props) {
       fixed="top"
       variant="dark"
       expand="lg"
-      style={{ background: "#0047a9" }}
+      style={{
+        background: "linear-gradient(to right, #2980B9, #87CEFA, #00BFFF)",
+        boxShadow: "0px 2px 5px rgba(0, 0, 0, 0.1)",
+      }}
     >
       <Container fluid>
         <Navbar.Brand>
@@ -39,6 +40,7 @@ function NavBar(props) {
               src={logo}
               width="75"
               className="d-inline-block align-top"
+              style={{ background: "#008000" }}
             />
           </LinkContainer>
         </Navbar.Brand>
@@ -51,18 +53,18 @@ function NavBar(props) {
             navbarScroll
           >
             <LinkContainer to="home">
-              <Nav.Link>Home</Nav.Link>
+              <Nav.Link style={{ transition: "color 0.3s" }}>Home</Nav.Link>
             </LinkContainer>
             {userType === "ADMIN" && loggedIn ? (
               <LinkContainer to="listings">
-                <Nav.Link>All Listings</Nav.Link>
+                <Nav.Link style={{ transition: "color 0.3s" }}>All Listings</Nav.Link>
               </LinkContainer>
             ) : (
               <div></div>
             )}
             {userType === "USER" && loggedIn ? (
               <LinkContainer to="listings">
-                <Nav.Link>Your Listings</Nav.Link>
+                <Nav.Link style={{ transition: "color 0.3s" }}>Your Listings</Nav.Link>
               </LinkContainer>
             ) : (
               <div></div>
@@ -70,13 +72,12 @@ function NavBar(props) {
 
             {userType === "USER" && loggedIn ? (
               <LinkContainer to="your-orders">
-                <Nav.Link>Your Orders</Nav.Link>
+                <Nav.Link style={{ transition: "color 0.3s" }}>Your Orders</Nav.Link>
               </LinkContainer>
             ) : (
               <div></div>
             )}
           </Nav>
-          {/*<Nav.Link href="#">Link</Nav.Link>*/}
 
           <Nav>
             <Form className="d-flex">
@@ -86,12 +87,28 @@ function NavBar(props) {
                 className="me-2"
                 aria-label="Search"
               />
-              <Button variant="success">Search</Button>
+              <Button
+                variant="success"
+                style={{
+                  background: "#E76F51",
+                  color: "#fff",
+                  padding: "10px 20px",
+                  border: "none",
+                  borderRadius: "20px", // Adjusted for round edges
+                  cursor: "pointer",
+                  boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
+                  transition: "background 0.3s, transform 0.3s, box-shadow 0.3s",
+                }}
+                onMouseOver={(e) => (e.target.style.background = "#FFA726")}
+                onMouseOut={(e) => (e.target.style.background = "#E76F51")}
+              >
+                Search
+              </Button>
             </Form>
 
             {userType === "USER" && loggedIn ? (
               <LinkContainer to="upload">
-                <Nav.Link>Add Book +</Nav.Link>
+                <Nav.Link style={{ transition: "color 0.3s" }}>Add Book +</Nav.Link>
               </LinkContainer>
             ) : (
               <div></div>
@@ -99,7 +116,7 @@ function NavBar(props) {
 
             {userType === "ADMIN" ? (
               <LinkContainer to="/all-accounts">
-                <Nav.Link>Accounts</Nav.Link>
+                <Nav.Link style={{ transition: "color 0.3s" }}>Accounts</Nav.Link>
               </LinkContainer>
             ) : (
               <div></div>
@@ -107,19 +124,15 @@ function NavBar(props) {
 
             {userType === "DELIVERY" ? (
               <LinkContainer to="/order-list">
-                <Nav.Link>Orders</Nav.Link>
+                <Nav.Link style={{ transition: "color 0.3s" }}>Orders</Nav.Link>
               </LinkContainer>
             ) : (
               <div></div>
             )}
 
-            {/* 
-              Adding conditional rendering depending if user is logged in
-            */}
-
             {loggedIn ? (
               <LinkContainer to="account-details">
-                <Nav.Link>My Account</Nav.Link>
+                <Nav.Link style={{ transition: "color 0.3s" }}>My Account</Nav.Link>
               </LinkContainer>
             ) : (
               <div></div>
@@ -129,15 +142,17 @@ function NavBar(props) {
               <div></div>
             ) : (
               <LinkContainer to="register">
-                <Nav.Link>Register</Nav.Link>
+                <Nav.Link style={{ transition: "color 0.3s" }}>Register</Nav.Link>
               </LinkContainer>
             )}
 
             {loggedIn ? (
-              <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+              <Nav.Link onClick={handleLogout} style={{ transition: "color 0.3s" }}>
+                Logout
+              </Nav.Link>
             ) : (
               <LinkContainer to="login">
-                <Nav.Link>Login</Nav.Link>
+                <Nav.Link style={{ transition: "color 0.3s" }}>Login</Nav.Link>
               </LinkContainer>
             )}
           </Nav>
