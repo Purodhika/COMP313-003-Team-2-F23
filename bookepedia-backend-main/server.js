ce08226deac3d78ab4b1d8b9d17aad1838b169a3
Purodhika Sharma <pshar245@my.centennialcollege.ca>
2023-12-06 17:15:19 -0500

    test server

--- bookepedia-backend-main/server.js
+++ bookepedia-backend-main/server.js
@@ -4,7 +4,7 @@ const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 const corsOptions = {
-  origin: "https://bookepedia-client.onrender.com/",
+  origin: "https://bookepedia-qta8.onrender.com/",
   credentials: true,
   optionSuccessStatus: 200,
 };
