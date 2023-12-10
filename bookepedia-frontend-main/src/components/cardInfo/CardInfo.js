3140eb1d5c6dc8822d1baa12e685decfdc3ea7e3
DESKTOP-AM0MM8O\Administrator <pgu1@my.centennialcollege.ca>
2023-12-10 12:40:13 -0500

    add validation for card number  full name address and postal code

--- bookepedia-frontend-main/src/components/cardInfo/CardInfo.js
+++ bookepedia-frontend-main/src/components/cardInfo/CardInfo.js
@@ -11,6 +11,10 @@ export default function CardInfo({ setPaymentValidated }) {
   const [name, setName] = useState("");
   const [address, setAddress] = useState("");
   const [postalCode, setPostalCode] = useState("");
+  const [isCardNumberValid, setIsCardNumberValid] = useState(true);
+  const [isNameValid, setIsNameValid] = useState(true);
+  const [isAddressValid, setIsAddressValid] = useState(true);
+  const [isPostalCodeValid, setIsPostalCodeValid] = useState(true);
 
   //const { loggedIn } = React.useContext(accountContext);
 
@@ -20,20 +24,43 @@ export default function CardInfo({ setPaymentValidated }) {
   //     navigate("/home");
   //   }
   // }, []);
+  function validateCardNumber(number) {
+    const re = /^[0-9]{16}$/;
+    return re.test(number);
+  }
+
+  function validateName(name) {
+    const re = /^[A-Za-z ]+$/;
+    return re.test(name);
+  }
+  
+  function validateAddress(address) {
+    const re = /^[A-Za-z0-9\s,.'-]{3,}$/;
+    return re.test(address);
+  }
+  
+  function validatePostalCode(postalCode) {
+    const re = /^[A-Za-z]\d[A-Za-z][ -]?\d[A-Za-z]\d$/;
+    return re.test(postalCode);
+  }
 
   function handleChange(event) {
     switch (event.target.name) {
       case "card-number":
         setCardNumber(event.target.value.toString());
+        setIsCardNumberValid(validateCardNumber(event.target.value.toString()));
         break;
       case "full-name":
         setName(event.target.value);
+        setIsNameValid(validateName(event.target.value));
         break;
       case "address":
         setAddress(event.target.value);
+        setIsAddressValid(validateAddress(event.target.value));
         break;
       case "postal-code":
         setPostalCode(event.target.value);
+        setIsPostalCodeValid(validatePostalCode(event.target.value));
         break;
       default:
         break;
@@ -63,6 +90,11 @@ export default function CardInfo({ setPaymentValidated }) {
             placeholder="XXXX XXXX XXXX XXXX"
             required={true}
           />
+           {!isCardNumberValid && (
+            <Form.Label style={{ color: 'red' }}>
+              Please enter a valid 16-digit card number
+            </Form.Label>
+          )}
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formFullName">
@@ -75,6 +107,11 @@ export default function CardInfo({ setPaymentValidated }) {
             placeholder="John Doe"
             required={true}
           />
+          {!isNameValid && (
+            <Form.Label style={{ color: 'red' }}>
+              Please enter a valid full name
+            </Form.Label>
+          )}
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formFullName">
@@ -87,6 +124,11 @@ export default function CardInfo({ setPaymentValidated }) {
             placeholder="1 Abernathy Rd"
             required={true}
           />
+            {!isAddressValid && (
+            <Form.Label style={{ color: 'red' }}>
+              Please enter a valid address
+            </Form.Label>
+          )}
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formFullName">
@@ -98,7 +140,12 @@ export default function CardInfo({ setPaymentValidated }) {
             onChange={handleChange}
             placeholder="A1A 1A1"
             required={true}
-          />
+          /> 
+          {!isPostalCodeValid && (
+            <Form.Label style={{ color: 'red' }}>
+              Please enter a valid postal code
+            </Form.Label>
+          )}
         </Form.Group>
 
         <Button variant="primary" type="submit">
